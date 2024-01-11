c0c87fea8bcb525a389f1f9ccf363542a77e2bf6
Tumohornblower <hornblower@tumo.world>
2024-01-11 17:42:02 +0100

    test

---
+++ classes.js
@@ -0,0 +1,518 @@
+class Grass {
+    constructor(x, y) {
+
+        this.x = x;
+        this.y = y;
+        this.colorCode = 1;
+        this.rounds = 0;
+        // Nachbarfelder
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+
+    findFields(symbol, symbol2 = 100) {
+        let found = [];
+        for (let i = 0; i < this.neighbors.length; i++) {
+            let pos = this.neighbors[i];
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posY >= 0 && posX < matrix[0].length && posY < matrix.length) {
+                if (matrix[posY][posX] === symbol || matrix[posY][posX] === symbol2) {
+                    found.push(pos);
+                }
+            }
+        }
+        return found;
+    }
+    mul() {
+        this.rounds += 1
+        if (this.rounds >= 6) {
+            let emptyFields = this.findFields(0, 5)
+            if (emptyFields.length > 0) {
+                let pos = random(emptyFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                grassArr.push(new Grass(newX, newY))
+                if (newX >= 0 && newY >= 0 && newX < 100 && newY < 100) {
+                    matrix[newY][newX] = this.colorCode
+                }
+            }
+            this.rounds = 0
+        }
+    }
+
+}
+
+class Fresser {
+    constructor(x, y) {
+        this.eaten = 0
+        this.notEaten = 0
+        this.x = x;
+        this.y = y;
+        this.colorCode = 2;
+        this.rounds = 0;
+        // Nachbarfelder
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+
+    findFields(symbol, symbol2 = 100) {
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+        let found = [];
+        for (let i = 0; i < this.neighbors.length; i++) {
+            let pos = this.neighbors[i];
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posY >= 0 && posX < matrix[0].length && posY < matrix.length) {
+                if (matrix[posY][posX] === symbol || matrix[posY][posX] === symbol2) {
+                    found.push(pos);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+        this.rounds += 1
+        if (this.rounds >= 1) {
+            let emptyFields = this.findFields(0, 5)
+            if (emptyFields.length > 0) {
+                let pos = random(emptyFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                matrix[this.y][this.x] = 0
+                matrix[newY][newX] = this.colorCode
+                this.x = newX
+                this
+                    .y = newY
+            }
+            this.rounds = 0
+        }
+    }
+    eat() {
+        this.rounds += 1
+        let returnTrue = false
+        if (this.rounds >= 1) {
+            let grassFields = this.findFields(1)
+            if (grassFields.length > 0) {
+                let pos = random(grassFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                matrix[this.y][this.x] = 0
+                matrix[newY][newX] = this.colorCode
+                this.x = newX
+                this.y = newY
+                for (let i = 0; i < grassArr.length; i++) {
+                    let grassObj = grassArr[i];
+                    if (grassObj.x === this.x && grassObj.y === this.y) {
+                        grassArr.splice(i, 1)
+                    }
+                }
+                returnTrue = true
+
+            }
+            this.rounds = 0
+        }
+        return returnTrue
+    }
+    mul() {
+        let returnEaten = false
+        if (this.eaten >= 5) {
+            let emptyFields = this.findFields(0, 5)
+            if (emptyFields.length > 0) {
+                returnEaten = true
+                let pos = random(emptyFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                fressArr.push(new Fresser(newX, newY))
+                matrix[newY][newX] = this.colorCode
+                this.eaten = 0
+            }
+        }
+        return returnEaten
+    }
+    resetEat() {
+        this.eaten = 0
+        this.notEaten++
+    }
+    plusEat() {
+        this.eaten++
+        this.notEaten = 0
+    }
+    die() {
+        if (this.notEaten >= 5) {
+            matrix[this.y][this.x] = 0
+            for (let i = 0; i < fressArr.length; i++) {
+                let fressObj = fressArr[i];
+                if (fressObj.x === this.x && fressObj.y === this.y) {
+                    fressArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+    }
+}
+class Predator {
+    constructor(x, y) {
+        this.eaten = 0
+        this.notEaten = 0
+        this.x = x;
+        this.y = y;
+        this.colorCode = 3;
+        this.rounds = 0;
+        // Nachbarfelder
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+
+    findFields(symbol, symbol2 = 100) {
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+        let found = [];
+        for (let i = 0; i < this.neighbors.length; i++) {
+            let pos = this.neighbors[i];
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posY >= 0 && posX < matrix[0].length && posY < matrix.length) {
+                if (matrix[posY][posX] === symbol || matrix[posY][posX] === symbol2) {
+                    found.push(pos);
+                }
+            }
+        }
+        return found;
+    } move() {
+        this.rounds += 1
+        if (this.rounds >= 1) {
+            let emptyFields = this.findFields(0, 5)
+            if (emptyFields.length > 0) {
+                let pos = random(emptyFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                matrix[this.y][this.x] = 0
+                matrix[newY][newX] = this.colorCode
+                this.x = newX
+                this.y = newY
+            }
+            this.rounds = 0
+        }
+    }
+    eat() {
+        this.rounds += 1
+        let returnTrue = false
+        if (this.rounds >= 1) {
+            let fresserfields = this.findFields(2)
+            if (fresserfields.length > 0) {
+                let pos = random(fresserfields)
+                let newX = pos[0]
+                let newY = pos[1]
+                matrix[this.y][this.x] = 0
+                matrix[newY][newX] = this.colorCode
+                this.x = newX
+                this.y = newY
+                for (let i = 0; i < fressArr.length; i++) {
+                    let fressObj = fressArr[i];
+                    if (fressObj.x === this.x && fressObj.y === this.y) {
+                        fressArr.splice(i, 1)
+                        break
+                    }
+                }
+                returnTrue = true
+
+            }
+            this.rounds = 0
+        }
+        return returnTrue
+    }
+    mul() {
+        let returnEaten = false
+        if (this.eaten >= 5) {
+            let emptyFields = this.findFields(0, 5)
+            if (emptyFields.length > 0) {
+                returnEaten = true
+                let pos = random(emptyFields)
+                let newX = pos[0]
+                let newY = pos[1]
+                fressArr.push(new Predator(newX, newY))
+                matrix[newY][newX] = this.colorCode
+                this.eaten = 0
+            }
+        }
+        return returnEaten
+    }
+    resetEat() {
+        this.eaten = 0
+        this.notEaten++
+    }
+    plusEat() {
+        this.eaten++
+        this.notEaten = 0
+    }
+    die() {
+        if (this.notEaten >= 8) {
+            matrix[this.y][this.x] = 0
+            for (let i = 0; i < predatorArr.length; i++) {
+                let preObj = predatorArr[i];
+                if (preObj.x === this.x && preObj.y === this.y) {
+                    predatorArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+    }
+}
+// Das Wasser wird zufällig auf dem Bildschirm gespawnt, es kennt seine Nachbarfelder und kann sich alle 6 Runden in max. 3 umliegende freiliegende Felder vermehren.
+// Die Farbe ist blau. Hat Gras um sich herum Wasser, so "frisst" es das Wasser. Gleichzeitig kann sich das Grasobjekt, das das Wasser "gefressen" hat in alle umliegenden, freien
+// Nachbarfelder vermehren. Das Wasser macht das gras also stärker
+class Water {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.colorCode = 4;
+        this.rounds = 0;
+        // Nachbarfelder
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+
+    findFields(symbol, symbol2 = 1000) {
+        let found = [];
+        for (let i = 0; i < this.neighbors.length; i++) {
+            let pos = this.neighbors[i];
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posY >= 0 && posX < matrix[0].length && posY < matrix.length) {
+                if (matrix[posY][posX] === symbol || matrix[posY][posX] === symbol2) {
+                    found.push(pos);
+                }
+            }
+        }
+        return found;
+    }
+    mul() {
+        this.rounds += 1
+        let grasfields = this.findFields(1)
+        if (this.rounds >= 6 && grasfields.length > 0) {
+            this.giessen(grasfields)
+            this.rounds = 0
+        }
+        else if (this.rounds >= 6) {
+            let emptyFields = this.findFields(0, 5)
+            for (let i = 0; i < emptyFields.length; i++) {
+                if (emptyFields.length >= 3 && i >= 3) {
+                    break
+                }
+                let pos = random(emptyFields)
+                emptyFields.splice(emptyFields.indexOf(pos))
+                let newX = pos[0]
+                let newY = pos[1]
+                waterArr.push(new Water(newX, newY))
+                matrix[newY][newX] = this.colorCode
+            }
+            this.rounds = 0
+        }
+    }
+    giessen(grassfields) {
+        let grassObj = random(grassfields)
+        let newX = this.x
+        let newY = this.y
+        grassArr.push(new Grass(newX, newY))
+
+        let removeElement = waterArr.indexOf(this)
+        if (removeElement > -1) {
+            waterArr.splice(removeElement, 1);
+            matrix[this.y][this.x] = 1
+        }
+        let emptyFields
+        for (let i = 0; i < grassArr.length; i++) {
+            let grass = grassArr[i];
+            if (grass.x === grassObj[0] && grass.y === grassObj[1]) {
+                grassObj = grassArr[i]
+                grassObj.rounds = 0
+            }
+        }
+
+        emptyFields = grassObj.findFields(0)
+
+        for (let i = 0; i < emptyFields.length; i++) {
+            let pos = emptyFields[i]
+            let newX = pos[0]
+            let newY = pos[1]
+            grassArr.push(new Grass(newX, newY))
+            matrix[newY][newX] = 1
+        }
+    }
+}
+
+
+// Der Meteorid ist kein Lebewesen im natürlichen Sinn. Er tritt zufällig im Spiel auf (nicht realistisch). Wenn er einschlägt, dann vernichtet er
+// andere Lebewesen. Seine Farbe ist braun. Nach dem Einschlag kann jedes Lebewesen wieder in Das Gebiet zurückkehren. Manchmal kommt es auch vor, dass
+// der Einschlag eine WasserQuelle freilegt.
+
+class Meteor {
+    constructor(x, y) {
+        this.waterquelle = false
+        if (randomNumber(0, 2) === 1) {
+            this.waterquelle = true
+        }
+        this.x = x;
+        this.y = y;
+        this.colorCode = 5;
+        // beinhaltete Felder
+        this.fields = [
+            [this.x, this.y],
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2],
+
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y - 1],
+            [this.x + 3, this.y + 1],
+            [this.x + 3, this.y - 1],
+            [this.x + 3, this.y],
+
+            [this.x - 2, this.y + 1],
+            [this.x - 3, this.y - 1],
+            [this.x - 3, this.y],
+            [this.x - 3, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 2, this.y - 1],
+            [this.x - 1, this.y - 3],
+            [this.x, this.y - 3],
+            [this.x + 1, this.y - 3],
+            [this.x - 1, this.y + 3],
+            [this.x + 1, this.y + 3],
+            [this.x + 1, this.y + 3],
+
+            [this.x + 1, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 3]
+
+        ];
+    }
+
+    einschlag() {
+        for (let i = 0; i < this.fields.length; i++) {
+            let pos = this.fields[i];
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posY >= 0 && posX < 100 && posY < 100) {
+                matrix[posY][posX] = this.colorCode;
+            }
+        }
+
+        if (waterArr.length !== 0) {
+            for (let i = 0; i < waterArr.length; i++) {
+                for (let j = 0; j < this.fields.length; j++) {
+                    if (waterArr[i] && waterArr[i].x === this.fields[j][0] && waterArr[i].y === this.fields[j][1]) {
+                        waterArr.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+        if (grassArr.length !== 0) {
+            for (let i = 0; i < grassArr.length; i++) {
+                for (let j = 0; j < this.fields.length; j++) {
+                    if (grassArr[i] && grassArr[i].x === this.fields[j][0] && grassArr[i].y === this.fields[j][1]) {
+                        grassArr.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+        if (fressArr.length !== 0) {
+            for (let i = 0; i < fressArr.length; i++) {
+                for (let j = 0; j < this.fields.length; j++) {
+                    if (fressArr[i] && fressArr[i].x === this.fields[j][0] && fressArr[i].y === this.fields[j][1]) {
+                        fressArr.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+        if (predatorArr.length !== 0) {
+            for (let i = 0; i < predatorArr.length; i++) {
+                for (let j = 0; j < this.fields.length; j++) {
+                    if (predatorArr[i] && predatorArr[i].x === this.fields[j][0] && predatorArr[i].y === this.fields[j][1]) {
+                        predatorArr.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+        if (randomNumber(0, 3) === 1 && this.x >= 0 && this.y >= 0 && this.x < 100 && this.y < 100) {
+            matrix[this.y][this.x] = 4;
+            waterArr.push(new Water(this.x, this.y));
+        }
+    }
+}
+
