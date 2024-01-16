457dd81225dbf88a00fc9539612436676880969e
Tumohornblower <hornblower@tumo.world>
2024-01-16 18:21:19 +0100

    session 3 Umgebung erstell

---
+++ Session 3/session 3.js
@@ -0,0 +1,14 @@
+let obj = {
+    "first_name": "Vardan",
+    "last_name": "Hovsepyan",
+    "age": 13,
+    "tumo_student": true,
+
+    sayHello(){
+        console.log(this.first_name)
+    }
+ }
+
+ console.log(obj);
+ console.log(obj.first_name);
+ obj.sayHello
