78420b6befef0c2dd2bbf69e9d983f3b7e83122c
Tumohornblower <hornblower@tumo.world>
2024-01-18 16:06:27 +0100

    Test

--- Game/classes.js
+++ Game/classes.js
@@ -7,7 +7,8 @@ class Lebewesen{
             [this.x, this.y - 1],
             [this.x + 1, this.y - 1],
             [this.x - 1, this.y],
-            [this.x + 1, this.y],
+            [this.x +
+                 1, this.y],
             [this.x - 1, this.y + 1],
             [this.x, this.y + 1],
             [this.x + 1, this.y + 1]
