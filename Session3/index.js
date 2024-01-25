89edeff084dbaf8974d1bc64e5669c1f34d4538d
Tumohornblower <hornblower@tumo.world>
2024-01-25 17:06:59 +0100

    Session3-Ordner: eigene Module erstellt

---
+++ Session3/index.js
@@ -0,0 +1,11 @@
+const Square = require("./square.js")
+
+let sq = new Square(10)
+let sq2 = new Square(20)
+let area = sq.getArea()
+let area2 = sq2.getArea()
+
+console.log(sq.side)
+console.log(area)
+console.log(area2)
+
