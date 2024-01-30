337fe3731af27d8cfd8bb42ca4761bc32999b7db
Tumohornblower <hornblower@tumo.world>
2024-01-30 18:11:28 +0100

    test

--- MeinServer/server.js
+++ MeinServer/server.js
@@ -23,5 +23,5 @@ app.use(function(req, res, next) {
   })
 
 app.listen(3000, function () {
-    console.log("Example is running on port 3000")
+    console.log("Example is running on port 3000!")
 })
